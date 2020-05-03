ce736144a5924e21032d2b0c9576f38591fd93be
Pham Hong Ky <hongkyhvnh@gmail.com>
2020-05-03 22:04:58 +0700

    Update loading exxcel

--- js/utils/uploadFile/convertExcel.js
+++ js/utils/uploadFile/convertExcel.js
@@ -5,6 +5,7 @@ const btnfire = document.querySelector(".upload")
 const collection = document.querySelector("#collection")
 const actionType = document.querySelector('#actionType')
 const progress = document.querySelector(".upload-progress")
+const storage = firebase.storage()
 
 var db = firebase.firestore();
 let newData =[]
@@ -14,21 +15,40 @@ var fileReader = new FileReader();
 inputFile.addEventListener('change', (e) => {
   excelFile = e.target.files[0]
 
-  if(excelFile) {
+   if(excelFile) {
     fileReader.onload = (e) => {
       var data = e.target.result;
       var workbook = XLSX.read(data, {type: "binary"})
       let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[workbook.SheetNames[0]])
       newData = [...rowObject]
-      showInTable(rowObject, result)
+      
+      newData.forEach(async (item, index) => {
+        const gsReference = storage.refFromURL(`gs://driving-test-exam.appspot.com/B2/${item.index}-1.jpg`)
+        console.log("item index", item.index)
+        console.log(`gs://driving-test-exam.appspot.com/B2/${item.index}-1.jpg`)
+        let img = ''
+        await gsReference.getDownloadURL().then(url => img = url).catch(error => img = null)
+        console.log("recieve img",img)
+        newData[index].images = img
+      })
+      showInTable(newData, result)
     }
   }
+  
   fileReader.readAsBinaryString(excelFile)
 })
 
 btnfire.addEventListener('click', () => {
   !excelFile && alert("Chưa upload file excel mà đòi upload  hả !!!!")
   !collection.value === "" && alert("Chưa điền đường link collection kìa ?")
+  
+  // covert location to url
+  newData.forEach(item => {
+      const gsReference = storage.refFromURL(`gs://driving-test-exam.appspot.com/B2/${item.index}-1.jpg`)
+      let img = ''
+      gsReference.getDownloadURL().then(url => img = url).catch(error => img = null)
+      item = {...item, img}
+  })
 
   let size = 0
   collection.value 
@@ -42,9 +62,8 @@ btnfire.addEventListener('click', () => {
         alert("Collection không tồn tại để cập nhật")
       } else {
         newData.forEach((item, index) => {
-          db.collection(collection.value).doc(`question${item.index}`).set({
+          db.collection(collection.value).doc(`question-${item.index}`).set({
             ...item,
-            images: null,
             createdAt: new Date()
           },{merge: true})
           .then(function() {
