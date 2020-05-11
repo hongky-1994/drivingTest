c227af87daa37b0a949a258b6adc17de00f37009
quan.trananh12@gmail.com <quan.trananh12@gmail.com>
2020-05-11 17:13:30 +0700

    chỉnh timer (chưa xong)

--- js/pages/exam/view.js
+++ js/pages/exam/view.js
@@ -149,9 +149,48 @@ const examView = {
         let structuredTest = document.querySelector(testSelector)
         examView.showScreen(testType)
         setTimeout(() => {
-            structuredTest.addEventListener("click", examView.showScreen(testType))            
+            structuredTest.addEventListener("click", examView.showScreen(testType)) 
+            examView.timer.showRemainingTime()           
         }, 2500)
     },
+
+    timer: {
+        endDate: new Date().getTime() + 20*60*1000,
+        calculateRemainingTime: () => {
+            let remainingTime 
+            let remainingMins
+            let remainingSecs
+            remainingTime = timer.endDate - new Date().getTime()
+            if( remainingTime > 0) {
+                remainingMins = Math.floor(remainingTime / (1000 * 60))
+                remainingSecs = Math.round((remainingTime - remainingMins * 60 *1000) / (1000)) 
+            }        
+            return [remainingMins, remainingSecs]
+        },
+        showRemainingTime: () => {
+            setInterval(() => {
+                let minute = document.querySelector("#minute")
+                let second = document.querySelector("#second")
+                minute.innerHTML = ""
+                second.innerHTML = ""
+                if (timer.calculateRemainingTime()[0] < 10) {
+                    minute.innerHTML += "0" + timer.calculateRemainingTime()[0]
+                }
+                else {
+                    minute.innerHTML += timer.calculateRemainingTime()[0]
+                }
+                if (timer.calculateRemainingTime()[1] < 10) {
+                    second.innerHTML += "0" + timer.calculateRemainingTime()[1]
+                }
+                else {
+                    second.innerHTML += timer.calculateRemainingTime()[1]
+                }
+                
+                }, 1000)
+        }
+    },
+    
+    
 
 
 }
